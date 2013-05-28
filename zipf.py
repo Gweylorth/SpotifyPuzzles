da29a8f7c471c56c32b02310b3afd70165310fdf
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2013-05-28 22:33:58 +0200

    Zipf's Song puzzle solved
    My solution for the puzzle "Zipf's Song", written in Python.
    Check what this problem is about here :
    https://www.spotify.com/us/jobs/tech/zipfsong/

---
+++ zipf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+""" A short script to sort songs according to their quality
+By Gwenegan Hudin
+16/01/2013
+gwenegan.hudin@insa-rennes.fr
+
+"""
+
+from operator import itemgetter
+import sys
+
+f = sys.stdin
+
+#Get number of songs in the album, and how many to pick
+m, n = [int(x) for x in f.readline().split()]
+        
+# Generate an array of tuples (quality, rank, songname)
+# rank will help to order songs with same quality by order of appearance
+
+songlist = []
+for i in range(1, m+1) :
+    song = f.readline().split()
+    count = long(song[0])
+    name = song[1]
+    quality = count*i
+    songlist.append((quality, m-i, name))
+
+f.close()
+
+# Sort the song list according to quality, then to rank
+songlist.sort(key = itemgetter(0,1))
+songlist.reverse()
+
+# Prints n songs
+for i in range(n) :
+    print songlist[i][2]
