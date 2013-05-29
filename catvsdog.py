c22e1a7cde7e42e77c7266ccc16f296e78a480b5
Gwenegan Hudin <gwenegan.hudin@insa-rennes.fr>
2013-05-29 11:05:11 +0200

    Second attempt - Wrong answer

--- catvsdog.py
+++ catvsdog.py
@@ -17,29 +17,30 @@ i = 0
 while i < cases:
     k = 0
     c, d, v = [int(x) for x in input.readline().split()]
-    votes = dict()
-    for j in range(0, c+d+1):
+    votes = dict()  # Will store how many upvotes a pet got
+    # Create all dictionary keys
+    for j in range(0, c+d):
         if j < c:
-            votes['C' + str(j+1)] = 1
+            votes['C' + str(j+1)] = 0
         else:
-            votes['D' + str(j-c+1)] = 1
+            votes['D' + str(j-c+1)] = 0
+    scores = votes.copy()  # Will store how much a pet is liked (up - down)
+
+    # For each vote, update the dictionaries
     while k < v:
         v1, v2 = [x for x in input.readline().split()]
         votes[v1] += 1
-        votes[v2] -= 1
+        scores[v1] += 1
+        scores[v2] -= 1
         k += 1
 
-    maxvotes = max([v for v in votes.itervalues()])
-    minvotes = min([v for v in votes.itervalues()])
-    print "Maxvotes : " + str(maxvotes)
-    if minvotes < 0:
-        minvotes = 0
-    print "Minvotes : " + str(minvotes)
-    happy = maxvotes - minvotes
-    if happy == 0:
-        happy = 1
-    results += str(happy) + "\n"
+    # How many votes got the pet with the best score?
+    max_score_pet = max(scores, key=scores.get)
+    max_vote = votes[max_score_pet]
+
+    # Update the results with the current session
+    results += str(max_vote) + "\n"
     i += 1
 
-print results
+print results.rstrip()
 input.close()
